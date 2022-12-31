6c813fd4860da0541170d0a023f83d3a3a367865
hj31 <119323397+hj6elice@users.noreply.github.com>
2022-12-31 16:13:23 +0900

    Delete 2212 directory

--- 2212/22/조해정_boj_10430.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-input = sys.stdin.readline()
-a, b, c = map(int, input.split())
-
-results = [(a+b)%c, ((a%c)+(b%c))%c, (a*b)%c, ((a%c)*(b%c))%c]
-for result in results:
-    print(result)
