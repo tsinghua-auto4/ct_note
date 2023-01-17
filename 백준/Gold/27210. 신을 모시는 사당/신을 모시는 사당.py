052ae02200033c722789b152ee97394607fe7b48
Kyunghoon Lee <54742536+kyunghoon196@users.noreply.github.com>
2023-01-17 23:41:08 +0900

    [Gold V] Title: 신을 모시는 사당, Time: 76 ms, Memory: 32300 KB -BaekjoonHub

--- 백준/Gold/27210. 신을 모시는 사당/신을 모시는 사당.py
+++ 백준/Gold/27210. 신을 모시는 사당/신을 모시는 사당.py
@@ -1,23 +1,32 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
 data = list(map(int, input().split()))
-prefix_sum_1 = [0] * n
-prefix_sum_2 = [0] * n
 
-prefix_sum_1[0] = 1 if data[0] == 1 else 0
-for i in range(1, n):
-    if data[i] == 1: 
-        prefix_sum_1[i] = prefix_sum_1[i-1]+1
-    else: 
-        prefix_sum_1[i] = prefix_sum_1[i-1]-1 if prefix_sum_1[i-1]-1 > 0 else 0
+prefix_sum_1 = 0
+prefix_sum_2 = 0
 
-prefix_sum_2[0] = 1 if data[0] == 2 else 0
-for i in range(1, n):
-    if data[i] == 2: 
-        prefix_sum_2[i] = prefix_sum_2[i-1]+1
-    else: 
-        prefix_sum_2[i] = prefix_sum_2[i-1]-1 if prefix_sum_2[i-1]-1 > 0 else 0
+max_1 = 0
+max_2 = 0
 
-print(max(max(prefix_sum_1), max(prefix_sum_2)))
+for d in data:
+    if d == 1:
+        prefix_sum_1 += 1
+    else:
+        if prefix_sum_1 < 1:
+            pass
+        else:
+            prefix_sum_1 -= 1
+    if prefix_sum_1 > max_1:
+        max_1 = prefix_sum_1
+
+for d in data:
+    if d == 2:
+        prefix_sum_2 += 1
+    else:
+        if prefix_sum_2 < 1:
+            pass
+        else:
+            prefix_sum_2 -= 1
+    if prefix_sum_2 > max_2:
+        max_2 = prefix_sum_2
+
+print(max_1 if max_1>max_2 else max_2 )
