16010260e1f6b9911f247ec0ab363901a4f096cf
Kyunghoon Lee <54742536+kyunghoon196@users.noreply.github.com>
2023-03-12 14:35:08 +0900

    add: 230312 boj_18809 revised (#165)
    * add: 230125 jungol_1303 solved
    
    * add: 230126 jungol_1856 solved
    
    * add: 230127 jungol_1304 solved
    
    * add: 230128 boj_10816 solved
    
    * add: 230129 jungol_2046 solved
    
    * add: 230130 jungol_1339 solved
    
    * 1
    
    * stash solved
    
    * add: 230131 jungol_1339 solved
    
    * add: 230202 boj_1764 solved
    
    * add 230203 jungol_1307 solved
    
    * add: 230204 jungol_1314 solved
    
    * add: 230205 boj_1085 solved
    
    * add: 230206 codility_lesson13_FibFrog solved
    
    * add: 230207 boj_1244 solved
    
    * add: 230208 boj_15649 solved
    
    * add: 230209 swea_1208, 1210 solved
    
    * add: 230211 swea_1954 solved
    
    * add: 230212 boj_1269 solved
    
    * add: 230212 boj_1914 solved
    
    * add: 230212 boj_2477 solved
    
    * add: 230212 boj_10814 solved
    
    * add: 230213 swea_1228 solved
    
    * add: 230213 boj_1158 solved
    
    * add: 230214 boj_24979, 27212, swea_1233 solved
    
    * add: boj_2493, 2563, 1260, jungol_1661 solved
    
    * add: swea_1861, 5215, programmers_92343 solved
    
    * add: swea_9229, jungol_2247, boj_2164, 3009, 3040 solved
    
    * add: boj_3053, 4153, 11478, 11659, 11660 sovled
    
    * add: 230220 boj_11725, 15650, 15661 solved
    
    * add: 230221 boj_15686, 17478 solved
    
    * add: 230222 boj_1074, 1987, 1992 solved
    
    * add: 230223 boj_1259, 2206, 7576 solved
    
    * add: 230224 boj_11050, 11866 solved
    
    * add: 230225 swea_5644, 1873, boj_11866 solved
    
    * add: 230226 boj_17135, 20301, swea 1949 solved
    
    * add: 230227 boj_1062, 1697, 1759 solved
    
    * add: 230228 boj_1920, 9012, 15829 solved
    
    * add: 230301 boj_10026, swea_3124 solved
    
    * add: 230301 boj_10026, swea_3124 solved
    
    * add: 230303 swea_1238, 3289, 7465 solved
    
    * add: 230304 boj_2629, 14442, 17471, swea_1753 solved
    
    * add: 230305 boj_10828, 10845, 10866 solved
    
    * add: 230306 boj_2567, 16236 solved
    
    * add: 230307 boj_4949, swea_1767, 2115 solved
    
    * add: 230307 boj_1541, 17143 solved
    
    * add: 230308 swea_5658 solved
    
    * add: 230309 boj_15661, 17471 solved
    
    * add: 230310 boj_11382 solved
    
    * add: 230311 boj_18809 solved
    
    * add: 230312 boj_18809 revised

--- BOJ/2303/11/이경훈_18809_Gaaaaaaaaaarden.py
+++ BOJ/2303/11/이경훈_18809_Gaaaaaaaaaarden.py
@@ -7,32 +7,36 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
+# 백트래킹으로 배양액의 위치 고름
 def chooseGround(cnt, idx, green, red, grounds):
   if green > G:
     return
   if red > R:
     return
-  if (G - green) + (R - red) > len(grounds) - idx:
+  if (G - green) + (R - red) > len(grounds) - idx:  # 남은 배양액보다 남은 땅이 더 적다면 종료
     return
-  if cnt == R + G:
-    bfs(chosen_ground)
-    return
-
-  if idx > len(grounds) - 1:
+  if cnt == R + G:  # 배양액을 뿌릴 땅 선정 완료
+    bfs(chosen_ground)  # bfs 돌림
     return
 
+  # 초록색 선택
   chosen_ground.append((grounds[idx], 1))
   chooseGround(cnt + 1, idx + 1, green + 1, red, grounds)
   chosen_ground.pop(-1)
+  # 빨간색 선택
   chosen_ground.append((grounds[idx], 2))
   chooseGround(cnt + 1, idx + 1, green, red + 1, grounds)
   chosen_ground.pop(-1)
+  # 미선택
   chooseGround(cnt, idx + 1, green, red, grounds)
 
 
 def bfs(chosen_ground):
   global ans
   _ans = 0
+  # visited[x][y][z] : x, y는 좌표 z는 [a, b]의 형태로서
+  # a는 상태를 의미함. 즉 0,1,2,3 각각 아무것도 없음, 초록색, 빨간색, 꽃을 의미함
+  # b는 시간을 의미함
   visited = [[[0 for _ in range(2)] for _ in range(M)] for _ in range(N)]
   q = deque()
 
@@ -42,26 +46,37 @@ def bfs(chosen_ground):
 
   while (q):
     x, y = q.popleft()
+    # 꽃이면 continue
     if visited[x][y][0] == 3: continue
     for i in range(4):
       nx, ny = x + dx[i], y + dy[i]
       if not inside(nx, ny): continue
       if garden[nx][ny] == 0: continue
 
+      # 만약 갈 칸에 아무 것도 없다면
       if visited[nx][ny][0] == 0:
+        # 현재 칸의 상태를 물려주고
         visited[nx][ny][0] = visited[x][y][0]
+        # 1의 시간을 더한다
         visited[nx][ny][1] = visited[x][y][1] + 1
+        # 큐에 넣어준다
         q.append((nx, ny))
 
+      # 만약 갈 칸에 초록색 배양액이 있다면
       elif visited[nx][ny][0] == 1:
+        # 만약 현재 칸에 빨간색 배양액이 있고 시간이 1초 차이라면
         if visited[x][y][0] == 2 and visited[nx][ny][1] == visited[x][y][1] + 1:
+          # 갈 칸을 꽃으로 만들어주고
           visited[nx][ny][0] = 3
+          # 결과값에 1 추가
           _ans += 1
 
+      # 위와 같다
       elif visited[nx][ny][0] == 2:
         if visited[x][y][0] == 1 and visited[nx][ny][1] == visited[x][y][1] + 1:
           visited[nx][ny][0] = 3
           _ans += 1
+  # 최대값으로 갱신
   ans = max(ans, _ans)
 
 
@@ -72,6 +87,7 @@ def inside(nx, ny):
     return False
 
 
+# 입력받기
 N, M, G, R = map(int, input().split())
 garden = []
 grounds = []
@@ -85,5 +101,6 @@ for i in range(N):
       grounds.append((i, j))
   garden.append(temp)
 
+# 메인함수 실행
 chooseGround(0, 0, 0, 0, grounds)
 print(ans)
