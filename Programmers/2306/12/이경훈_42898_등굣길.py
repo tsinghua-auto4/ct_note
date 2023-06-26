b9b755220fd9618e6c78d6ba82f553afb653307a
kyunghoon196 <coelee5@naver.com>
2023-06-27 01:41:14 +0900

    add: 230627 Programmers_42898 solved

---
+++ Programmers/2306/12/이경훈_42898_등굣길.py
@@ -0,0 +1,11 @@
+def solution(m, n, puddles):
+    dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+    dp[1][1] = 1
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i==1 and j==1:
+                continue
+            if [j,i] in puddles:
+                continue
+            dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000007
+    return dp[n][m]
