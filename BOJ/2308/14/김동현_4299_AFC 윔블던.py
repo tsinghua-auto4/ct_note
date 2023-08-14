3db1db7b38e4c858bf2bdd3a13147069c5a8265d
dhx <dhx>
2023-08-14 23:59:47 +0900

    fix: 230814 boj_4299 Fixed

--- BOJ/2308/14/김동현_4299_AFC 윔블던.py
+++ BOJ/2308/14/김동현_4299_AFC 윔블던.py
@@ -1,11 +1,7 @@
-a, b = map(int, input().split())
-A = (a+b)//2
-B = int(a-A)
-
-if A >= 0 and B >= 0:
-    if A > B:
-        print(A, B)
-    else:
-        print(B, A)
-if A<0 or B<0 or (a+b)%2:
+s, m = map(int, input().split())
+if  s+m < 0 or s-m < 0 or (s + m) % 2:
     print(-1)
+else:
+    a = (s + m) // 2
+    b = s - a
+    print(max(a, b), min(a, b))
